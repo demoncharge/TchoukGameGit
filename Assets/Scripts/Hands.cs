797f1f50a31d9c52223a6cca75f93e3287446bb4
demoncharge <168311406+demoncharge@users.noreply.github.com>
2025-01-19 03:15:42 +0800

    push

--- Assets/Scripts/Hands.cs
+++ Assets/Scripts/Hands.cs
@@ -18,19 +18,15 @@ public class Hands : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             Height += (1*Speed/50);
-            Debug.Log("UP");
-            Debug.Log(Height);
         }
         if (Input.GetKey(KeyCode.S))
         {
             Height -= (1*Speed/50);
-            Debug.Log("DOWN");
         }
         transform.position = new Vector3(transform.position.x, Height, transform.position.z);
         Vector2 a = new Vector2(transform.position.x, transform.position.y);
         Vector2 b = new Vector2(0, 1);
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 57 * Mathf.Atan(7 * Height) - 90);
-        Debug.Log(Mathf.Tan(Vector2.Distance(a,b)));
     }
 
     void ChangeSprite(Sprite sprite)
