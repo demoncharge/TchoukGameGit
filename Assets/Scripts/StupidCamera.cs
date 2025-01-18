ac0f8fabecc198e63244463f742bc65356ec6817
yugyhjuh <stephanie521404@gmail.com>
2025-01-19 01:57:57 +0800

    Update StupidCamera.cs

--- Assets/Scripts/StupidCamera.cs
+++ Assets/Scripts/StupidCamera.cs
@@ -7,19 +7,17 @@ public class StupidCamera : MonoBehaviour
     public float Angle = 90;
     public float Speed = 30;
     public float Radius = 3.1f;
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    // Update is called once per 0.02s
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.A)) 
@@ -32,9 +30,10 @@ public class StupidCamera : MonoBehaviour
             Angle += (1 * Speed) / 50;
         }
 
+        Angle = Mathf.Clamp(Angle, 30f, 150f);
+
         Vector3 DefenderPosition = new Vector3((float) Radius * Mathf.Sin(Angle * (Mathf.PI/180)), (float) Radius * Mathf.Cos(Angle * (Mathf.PI/180)), 1);
         transform.position = new Vector3(-DefenderPosition.y, DefenderPosition.z, -DefenderPosition.x);
-        transform.eulerAngles = new Vector3(0, 90-Angle, 0);
-
+        transform.eulerAngles = new Vector3(0, 90 - Angle, 0);
     }
 }
